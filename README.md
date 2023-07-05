55d48b78a289b74771aed59b269410ca104c7fb1
stiva98 <samustiva9810@gmail.com>
2023-07-05 16:33:53 +0200

    ADD generato 10 email

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# template-html-css-js-basic
+Descrizione:
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista. OK
+
+
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            email: []
+        }
+    },
+
+    created() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                const result = response.data
+                this.email.push(result.response)
+            })
+            
+        }
+    }
+
+}).mount('#app')
